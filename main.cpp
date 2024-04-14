553616aa4cf35dfdac3a159c0a57eff34ab87c7e
James Wu <jameswu62@yahoo.co.nz>
2024-04-14 17:17:42 +1000

    add code to received ethernet data

--- main.cpp
+++ main.cpp
@@ -2,12 +2,26 @@
 #include <cstdlib>
 #include "net_socket.hpp"
 
+#include <array>
+#include <cstdint>
+
 int main()
 {
     std::printf("Hello world\r\n");
     net::socket_raw_rx socket_raw_rx{};
 
     std::printf("socket df: %i\r\n", socket_raw_rx.id().value_or(-1));
+
+    std::array<std::uint8_t, 65000> buffer{};
+    struct sockaddr saddr;
+    int saddr_len = sizeof(saddr);
+    const auto rx_len = recvfrom(socket_raw_rx.id().value(),
+                                 buffer.data(),
+                                 buffer.size(),
+                                 0,
+                                 &saddr, (socklen_t *)&saddr_len);
+
+    std::printf("rx len: %lu\r\n", rx_len);
 
     return EXIT_SUCCESS;
 }
