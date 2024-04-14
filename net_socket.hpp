c72e5f61d826b42675cb603ecc8a78a75e395774
James Wu <jameswu62@yahoo.co.nz>
2024-04-14 17:52:12 +1000

    add file comments

--- net_socket.hpp
+++ net_socket.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file net_socket.hpp
+ * @author James Wu (jameswu62@yahoo.co.nz)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #pragma once
 
 #include <optional>
