dd73650934786ce9ab7924a45740f52dbe63a59d
James Wu <jameswu62@yahoo.co.nz>
2024-04-14 17:52:21 +1000

    add file comments

---
+++ net_receiver.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file net_receiver.hpp
+ * @author James Wu (jameswu62@yahoo.co.nz)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
